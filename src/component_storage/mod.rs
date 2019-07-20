66c0d00aba37c7997babd6136c0d888ccc1763e0
leudz <dylan.ancel@gmail.com>
2019-07-21 00:31:53 +0200

    add get_storage

--- src/component_storage/mod.rs
+++ src/component_storage/mod.rs
@@ -2,6 +2,7 @@ mod hasher;
 
 use crate::atomic_refcell::{AtomicRefCell, Ref, RefMut};
 use crate::error;
+use crate::get_storage::GetStorage;
 use crate::sparse_array::SparseArray;
 use hasher::TypeIdHasher;
 use std::any::{Any, TypeId};
@@ -9,15 +10,15 @@ use std::collections::HashMap;
 use std::hash::BuildHasherDefault;
 
 /// Wrapper over `AtomicRefCell<Box<SparseArray<T>>>` to be able to store different `T`s
-/// in a `HashMap<TypeId, ComponentStorage>`
-pub struct ComponentStorage(AtomicRefCell<Box<dyn Any + Send + Sync>>);
+/// in a `HashMap<TypeId, ComponentStorage>`.
+pub(crate) struct ComponentStorage(AtomicRefCell<Box<dyn Any + Send + Sync>>);
 
 impl ComponentStorage {
-    /// Creates a new `ComponentStorage` storing elements of type T
+    /// Creates a new `ComponentStorage` storing elements of type T.
     pub(crate) fn new<T: 'static + Send + Sync>() -> Self {
         ComponentStorage(AtomicRefCell::new(Box::new(SparseArray::<T>::default())))
     }
-    /// Immutably borrows the container
+    /// Immutably borrows the container.
     pub(crate) fn array<'a, T: 'static>(
         &'a self,
     ) -> Result<Ref<'a, SparseArray<T>>, error::Borrow> {
@@ -25,7 +26,7 @@ impl ComponentStorage {
             array.downcast_ref().unwrap()
         }))
     }
-    /// Mutably borrows the container
+    /// Mutably borrows the container.
     pub(crate) fn array_mut<'a, T: 'static>(
         &'a self,
     ) -> Result<RefMut<'a, SparseArray<T>>, error::Borrow> {
@@ -35,7 +36,7 @@ impl ComponentStorage {
     }
 }
 
-/// Contains all components present in the World
+/// Contains all components present in the World.
 // Wrapper to hide `TypeIdHasher` and the whole `HashMap` from public interface
 #[derive(Default)]
 pub struct AllStorages(
@@ -43,11 +44,39 @@ pub struct AllStorages(
 );
 
 impl AllStorages {
-    /// Add a storage for a new type
-    /// Does nothing if a storage already exists
-    pub fn add_storage<T: 'static + Send + Sync>(&mut self) {
+    /// Register a new component type and create a storage for it.
+    /// Does nothing if a storage already exists.
+    pub fn register<T: 'static + Send + Sync>(&mut self) {
         self.0
             .entry(TypeId::of::<T>())
             .or_insert_with(|| ComponentStorage::new::<T>());
+    }
+}
+
+impl<'a> Ref<'a, AllStorages> {
+    /// Same as `try_get_storage` but will `unwrap` if an error occurs
+    pub fn get_storage<T: GetStorage<'a>>(&self) -> T::Storage {
+        self.try_get_storage::<T>().unwrap()
+    }
+    /// Retrives storages based on type `T`.
+    /// `&T` returns a read access to the storage.
+    /// `&mut T` returns a write access to the storage.
+    /// To retrive multiple storages at once, use a tuple.
+    pub fn try_get_storage<T: GetStorage<'a>>(&self) -> Result<T::Storage, error::GetStorage> {
+        T::get_in(Ref::clone(self))
+    }
+}
+
+impl<'a> RefMut<'a, AllStorages> {
+    /// Same as `try_get_storage` but will `unwrap` if an error occurs
+    pub fn get_storage<T: GetStorage<'a>>(self) -> T::Storage {
+        self.try_get_storage::<T>().unwrap()
+    }
+    /// Retrives storages based on type `T`.
+    /// `&T` returns a read access to the storage.
+    /// `&mut T` returns a write access to the storage.
+    /// To retrive multiple storages at once, use a tuple.
+    pub fn try_get_storage<T: GetStorage<'a>>(self) -> Result<T::Storage, error::GetStorage> {
+        RefMut::downgrade(self).try_get_storage::<T>()
     }
 }
---
+++ src/get_storage.rs
@@ -0,0 +1,79 @@
+use crate::atomic_refcell::Ref;
+use crate::component_storage::AllStorages;
+use crate::error;
+use crate::sparse_array::{Read, Write};
+use std::any::TypeId;
+use std::convert::TryInto;
+
+// `GetStorage` allows to retrive component storages
+// A storage can be found based on the `TypeId` of the input
+// Mutability is determined by `&` for read access and `&mut` for write access
+pub trait GetStorage<'a> {
+    type Storage;
+    fn get_in(storages: Ref<'a, AllStorages>) -> Result<Self::Storage, error::GetStorage>;
+}
+
+pub trait AbstractMut<'a> {
+    type Out;
+    fn borrow(storages: Ref<'a, AllStorages>) -> Result<Self::Out, error::GetStorage>;
+}
+
+impl<'a, T: 'static> AbstractMut<'a> for &T {
+    type Out = Read<'a, T>;
+    fn borrow(storages: Ref<'a, AllStorages>) -> Result<Self::Out, error::GetStorage> {
+        Ref::try_map(storages, |storages| {
+            match storages.0.get(&TypeId::of::<T>()) {
+                Some(storage) => Ok(storage),
+                None => Err(error::GetStorage::MissingComponent),
+            }
+        })?
+        .try_into()
+        .map_err(|err| error::GetStorage::StorageBorrow(err))
+    }
+}
+
+impl<'a, T: 'static> AbstractMut<'a> for &mut T {
+    type Out = Write<'a, T>;
+    fn borrow(storages: Ref<'a, AllStorages>) -> Result<Self::Out, error::GetStorage> {
+        Ref::try_map(storages, |storages| {
+            match storages.0.get(&TypeId::of::<T>()) {
+                Some(storage) => Ok(storage),
+                None => Err(error::GetStorage::MissingComponent),
+            }
+        })?
+        .try_into()
+        .map_err(|err| error::GetStorage::StorageBorrow(err))
+    }
+}
+
+impl<'a, T: AbstractMut<'a>> GetStorage<'a> for T {
+    type Storage = T::Out;
+    fn get_in(storages: Ref<'a, AllStorages>) -> Result<Self::Storage, error::GetStorage> {
+        Ok(T::borrow(storages)?)
+    }
+}
+
+macro_rules! impl_get_storage {
+    ($(($type: ident, $index: tt))+) => {
+        impl<'a, $($type: AbstractMut<'a>),+> GetStorage<'a> for ($($type,)+) {
+            type Storage = ($($type::Out,)+);
+            fn get_in(storages: Ref<'a, AllStorages>) -> Result<Self::Storage, error::GetStorage> {
+                Ok(($(
+                    $type::borrow(Ref::clone(&storages))?,
+                )+))
+            }
+        }
+    }
+}
+
+macro_rules! get_storage {
+    ($(($left_type: ident, $left_index: tt))*;($type1: ident, $index1: tt) $(($type: ident, $index: tt))*) => {
+        impl_get_storage![$(($left_type, $left_index))*];
+        get_storage![$(($left_type, $left_index))* ($type1, $index1); $(($type, $index))*];
+    };
+    ($(($type: ident, $index: tt))*;) => {
+        impl_get_storage![$(($type, $index))*];
+    }
+}
+
+get_storage![(A, 0); (B, 1) (C, 2) (D, 3) (E, 4) /*(F, 5) (G, 6) (H, 7) (I, 8) (J, 9) (K, 10) (L, 11)*/];
