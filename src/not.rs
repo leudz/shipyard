184270282073b48e5ea9694b994ffda9999ed525
leudz <dylan.ancel@gmail.com>
2019-08-02 20:29:05 +0200

    add Not::into_inner modify Not.0 visibility

--- src/not.rs
+++ src/not.rs
@@ -4,7 +4,13 @@ use std::ops::Not as NotOps;
 // Used to filter out components
 // Get and iterators will skip entities that have this component
 #[derive(Copy, Clone)]
-pub struct Not<T>(pub T);
+pub struct Not<T>(pub(crate) T);
+
+impl<T> Not<T> {
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
 
 impl<T> NotOps for Read<'_, T> {
     type Output = Not<Self>;
