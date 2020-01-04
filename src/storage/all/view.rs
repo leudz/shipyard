957a41d671600a6fef88fbb486607635946ba0d6
Dylan Ancel <dylan.ancel@gmail.com>
2020-01-04 15:22:33 +0100

    add AllStoragesViewMut::strip

--- src/storage/all/view.rs
+++ src/storage/all/view.rs
@@ -41,30 +41,34 @@ impl AllStoragesViewMut<'_> {
         if entities.delete(entity) {
             drop(entities);
 
-            let mut storage_to_unpack = Vec::new();
-
-            for storage in self.0.values_mut() {
-                let observers = storage.delete(entity).unwrap();
-                storage_to_unpack.reserve(observers.len());
-
-                let mut i = 0;
-                for observer in observers.iter().copied() {
-                    while i < storage_to_unpack.len() && observer < storage_to_unpack[i] {
-                        i += 1;
-                    }
-                    if storage_to_unpack.is_empty() || observer != storage_to_unpack[i] {
-                        storage_to_unpack.insert(i, observer);
-                    }
-                }
-            }
-
-            for storage in storage_to_unpack {
-                self.0.get_mut(&storage).unwrap().unpack(entity).unwrap();
-            }
+            self.strip(entity);
 
             true
         } else {
             false
+        }
+    }
+    /// Deletes all components from an entity without deleting it.
+    pub fn strip(&mut self, entity: EntityId) {
+        let mut storage_to_unpack = Vec::new();
+
+        for storage in self.0.values_mut() {
+            let observers = storage.delete(entity).unwrap();
+            storage_to_unpack.reserve(observers.len());
+
+            let mut i = 0;
+            for observer in observers.iter().copied() {
+                while i < storage_to_unpack.len() && observer < storage_to_unpack[i] {
+                    i += 1;
+                }
+                if storage_to_unpack.is_empty() || observer != storage_to_unpack[i] {
+                    storage_to_unpack.insert(i, observer);
+                }
+            }
+        }
+
+        for storage in storage_to_unpack {
+            self.0.get_mut(&storage).unwrap().unpack(entity).unwrap();
         }
     }
     pub fn register<T: 'static + Send + Sync>(&mut self) {
