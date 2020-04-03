3269f63215ecd57710054327807f55a9363ac6a4
leudz <dylan.ancel@gmail.com>
2020-04-03 09:11:36 +0200

    add test for different modules

--- tests/derive/good.rs
+++ tests/derive/good.rs
@@ -12,4 +12,15 @@ fn run(_: &mut Entities, _: &mut u32) {}
 #[system(Lifetime)]
 fn run(_: &'a usize, _: &'b mut i32, _: &'c Entities, _: Unique<&'d u32>, _: Entities) {}
 
+#[system(Modules)]
+fn run(_: &mut mod1::Test, _: &mut mod2::Test) {}
+
 fn main() {}
+
+mod mod1 {
+    pub struct Test;
+}
+
+mod mod2 {
+    pub struct Test;
+}
