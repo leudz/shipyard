999aba0137e5c8b1b7b6475572f69b9c0131274c
leudz <dylan.ancel@gmail.com>
2020-04-03 12:28:38 +0200

    re-export StorageBorrow

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,29 +1,33 @@
+pub use crate::delete::Delete;
 pub use crate::get::GetComponent;
 pub use crate::iter::{CurrentId, ExactSizeShiperator, IntoIter, IntoIterIds, Shiperator};
 pub use crate::not::Not;
+pub use crate::pack::{LoosePack, TightPack};
 pub use crate::remove::Remove;
-pub use crate::run::System;
 #[doc(hidden)]
 pub use crate::run::{FakeBorrow, SystemData};
+pub use crate::run::{StorageBorrow, System};
 pub use crate::sparse_set::sort::IntoSortable;
+pub use crate::sparse_set::{Window, WindowMut};
 pub use crate::storage::{AllStorages, Entities, EntitiesMut, EntityId};
 pub use crate::views::{
     AllStoragesViewMut, EntitiesView, EntitiesViewMut, UniqueView, UniqueViewMut, View, ViewMut,
 };
 pub use crate::world::World;
 pub use crate::Unique;
+
+#[cfg(feature = "non_send")]
+pub use crate::NonSend;
+
+#[cfg(feature = "non_sync")]
+pub use crate::NonSync;
+
+#[cfg(all(feature = "non_send", feature = "non_sync"))]
+pub use crate::NonSendSync;
+
+#[cfg(feature = "parallel")]
+pub use crate::ThreadPool;
+
 #[doc(hidden)]
 #[cfg(feature = "proc")]
 pub use shipyard_proc::system;
-
-pub use crate::delete::Delete;
-pub use crate::pack::{LoosePack, TightPack};
-pub use crate::sparse_set::{Window, WindowMut};
-#[cfg(feature = "non_send")]
-pub use crate::NonSend;
-#[cfg(all(feature = "non_send", feature = "non_sync"))]
-pub use crate::NonSendSync;
-#[cfg(feature = "non_sync")]
-pub use crate::NonSync;
-#[cfg(feature = "parallel")]
-pub use crate::ThreadPool;
