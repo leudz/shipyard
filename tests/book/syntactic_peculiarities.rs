454288572c27bfbdf57cf601bb3b8539c4486692
Cole Lawrence <cole@colelawrence.com>
2020-12-22 14:00:12 -0500

    clippy: Allow `clippy::toplevel_ref_arg` for syntax_peculiarities

--- tests/book/syntactic_peculiarities.rs
+++ tests/book/syntactic_peculiarities.rs
@@ -1,12 +1,14 @@
 use shipyard::*;
 
 #[allow(unused)]
+#[allow(clippy::toplevel_ref_arg)]
 fn ref_mut(ref mut u32s: ViewMut<u32>) {
     let id = EntityId::dead();
     let i: Mut<u32> = u32s.get(id).unwrap();
 }
 
 #[allow(unused)]
+#[allow(clippy::toplevel_ref_arg)]
 fn ref_sys(ref u32s: View<u32>, ref usizes: View<usize>) {
     let id = EntityId::dead();
     (usizes, u32s).get(id).unwrap();
