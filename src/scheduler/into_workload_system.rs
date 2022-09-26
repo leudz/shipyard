3702e8e43f3257d6bac0342513a568d95b89b803
notinmybackyaard <theloder@naver.com>
2022-09-26 10:21:57 +0900

    Extend into_workload_system param

--- src/scheduler/into_workload_system.rs
+++ src/scheduler/into_workload_system.rs
@@ -190,4 +190,7 @@ macro_rules! into_workload_system {
     }
 }
 
-into_workload_system![(A, 0); (B, 1) (C, 2) (D, 3) (E, 4) (F, 5) (G, 6) (H, 7) (I, 8) (J, 9)];
+into_workload_system![
+    (A, 0); (B, 1) (C, 2) (D, 3) (E, 4) (F, 5) (G, 6) (H, 7) (I, 8) (J, 9)
+    (K, 10) (L, 11) (M, 12) (N, 13) (O, 14) (P, 15) (Q, 16) (R, 17) (S, 18) (T, 19)
+];
