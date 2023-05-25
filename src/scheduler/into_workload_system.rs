cf8aaa24e40f53dfba5ff0e94ec94d92701d8544
notinmybackyaard <theloder@naver.com>
2023-05-25 16:19:38 +0900

    Expand parameter limit to 30

--- src/scheduler/into_workload_system.rs
+++ src/scheduler/into_workload_system.rs
@@ -193,4 +193,5 @@ macro_rules! into_workload_system {
 into_workload_system![
     (A, 0); (B, 1) (C, 2) (D, 3) (E, 4) (F, 5) (G, 6) (H, 7) (I, 8) (J, 9)
     (K, 10) (L, 11) (M, 12) (N, 13) (O, 14) (P, 15) (Q, 16) (S, 17) (T, 18) (U, 19)
+    (V, 20) (W, 21) (X, 22) (Y, 23) (Z, 24) (AA, 25) (AB, 26) (AC, 27) (AD, 28) (AE, 29)
 ];
--- src/scheduler/system_modificator.rs
+++ src/scheduler/system_modificator.rs
@@ -360,4 +360,5 @@ macro_rules! into_workload_system {
 into_workload_system![
     (A, 0); (B, 1) (C, 2) (D, 3) (E, 4) (F, 5) (G, 6) (H, 7) (I, 8) (J, 9)
     (K, 10) (L, 11) (M, 12) (N, 13) (O, 14) (P, 15) (Q, 16) (S, 17) (U, 18) (V, 19)
+    (W, 20) (X, 21) (Y, 22) (Z, 23) (AA, 24) (AB, 25) (AC, 26) (AD, 27) (AE, 28) (AF, 29)
 ];
