729c8e245c3d95a639d45e9555b709381eb6219c
leudz <dylan.ancel@gmail.com>
2024-02-09 15:10:35 +0100

    Add Send + Sync bounds when thread_local feature is disabled
    Fix #189

--- src/component.rs
+++ src/component.rs
@@ -1,5 +1,13 @@
 /// Indicates that a `struct` or `enum` can be store in the `World`.
+#[cfg(feature = "thread_local")]
 pub trait Component: Sized + 'static {}
+/// Indicates that a `struct` or `enum` can be store in the `World`.
+#[cfg(not(feature = "thread_local"))]
+pub trait Component: Sized + Send + Sync + 'static {}
 
 /// Indicates that a `struct` or `enum` can be store a single time in the `World`.
+#[cfg(feature = "thread_local")]
 pub trait Unique: Sized + 'static {}
+/// Indicates that a `struct` or `enum` can be store a single time in the `World`.
+#[cfg(not(feature = "thread_local"))]
+pub trait Unique: Sized + Send + Sync + 'static {}
